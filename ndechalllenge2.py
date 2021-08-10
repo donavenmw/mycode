35a351a97f0d9cc71385e82a652d277324379bca
Donaven Williams <donavenmw@users.noreply.github.com>
2021-08-10 16:33:16 +0000

    learning about dictionaries

---
+++ ndechalllenge2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+dogs = ["1 dalmation", 5, "3 huskies", ["spot", "toto", "kujo", "dex", "fred"], "1 St. Bernard"]
+Print(f"I have {dogs[1] dogs. My {dogs[2]} are {[dogs[3][0]}, {dogs[3][2]}, {dogs[3][3]}. My {dogs[0]} is {dogs[3][1]} and my {dogs[4]} is {dogs[3][4]}"
+
