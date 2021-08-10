5bf333638f4af461e097052a83ea613edec6db41
Donaven Williams <donavenmw@users.noreply.github.com>
2021-08-10 18:44:23 +0000

    join and split

---
+++ ndechallenges/ndechallenge3.py
@@ -0,0 +1,4 @@
+ #!/usr/bin/env python3
+pets = {"cats": ["fluffy", "snowball", "garfield"], "dogs": ["toto", "kujo", "spot"]}
+print(f"I have the cats {pets['cats'][0]},{pets['cats'][1]},{pets['cats'][2]}, and the dogs {pets['dogs'][0]},{pets['dogs'][1]},{pets['dogs'][2]}.")
+
