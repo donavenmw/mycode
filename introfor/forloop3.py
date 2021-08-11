723c30df9e61ace51488aee9addb59682c5bad68
Donaven Williams <donavenmw@users.noreply.github.com>
2021-08-11 21:23:44 +0000

    Outputting files

---
+++ introfor/forloop3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""RZFeeser | Alta3 Research
+   Printing dictionary data stored as lists to the screen"""
+print(__name__)
+if __name__ == "__main__":
+    farms()
+
+def farms():
+    """And on that farm he had a..."""
+
+# create a list of strings
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]}
+# create a second list of strings
+ {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]}
